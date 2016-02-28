3e1dabf34a7fb23b26bdf1ee8dc444a140b3f132
MeiliChen <102316218@gms.tcu.edu.tw>
2016-02-28 22:18:33 +0800

    七言絕句

---
+++ Seven.java
@@ -0,0 +1,9 @@
+
+public class Seven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("賀知章回鄉偶書\n少小離家老大回\n鄉音無改鬢毛衰\n兒童相見不相識\n笑問客從何處來");
+	}
+
+}
